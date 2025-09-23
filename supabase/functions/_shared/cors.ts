7599507933259657abe21c9a4c2e22130e020366
jrulysesbz <augustinejrulyses@gmail.com>
2025-09-23 12:49:13 +0800

    edge: add CORS header module (lock to localhost:3000)

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,5 @@
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Headers': 'authorization,content-type',
+  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+};
